288be0fbb71565a247e047fe983582f5573dcc10
cheapshops <arun2466@gmail.com>
2019-04-13 16:04:49 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import Auth from './containers/Auth'
+import Home from './containers/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home}/>
+          <Route path="/auth" component={Auth}/>
+        </Switch>
+      </Router>
     );
   }
 }

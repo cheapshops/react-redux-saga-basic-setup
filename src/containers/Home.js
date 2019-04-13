943cee583cc712360d1e0d1fac3427c86a89891a
cheapshops <arun2466@gmail.com>
2019-04-13 16:14:17 +0530

    Final - action called from component and update reducer

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { loginRequest } from '../redux/actions'
 
 class Home extends Component {
+
+  fireAction = () => {
+    this.props.loginRequest({
+      username:'test',
+      password:'test'
+    })
+  }
+
   render() {
     return (
-      <h1> HOME </h1>
+      <div>
+        <h1> HOME </h1>
+        <input type="button" value="Fire Action" onClick={this.fireAction}/>
+      </div>
     )
   }
 }
 
-export default Home
+const mapStateToProps = state => ({
+  login: state.login
+})
+
+const mapDisptachToProps = dispatch => ({
+  loginRequest: (data) => dispatch( loginRequest(data))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDisptachToProps
+)( Home )

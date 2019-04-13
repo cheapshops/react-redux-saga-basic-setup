51eb6100937a0427dd9e5314cac4eaa22c118ed4
cheapshops <arun2466@gmail.com>
2019-04-13 15:35:23 +0530

    created saga

---
+++ src/redux/saga.js
@@ -0,0 +1,12 @@
+import { takeLatest } from 'redux-saga/effects'
+import constants from '..constants'
+
+import { loginRequest } from './login/action'
+
+function* watchActions() {
+  yield takeLatest( constants.LOGIN_REQUEST, loginRequest)
+}
+
+export default function* rootSaga() {
+  yield watchActions()
+}
